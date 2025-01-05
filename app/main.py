663c73a6e2be8439481d138527e2087d55e4e363
mrjl <mrjl@softleg.pl>
2025-01-05 19:58:06 +0100

    Add templating

--- app/main.py
+++ app/main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import FileResponse
+from fastapi.templating import Jinja2Templates
+from fastapi import Request
 from .database import engine, Base
 from .endpoints import sewage
 import os
@@ -14,7 +16,10 @@ app.include_router(sewage.router)
 # Mount the static files directory
 app.mount("/ui", StaticFiles(directory="ui"), name="ui")
 
+# Set up Jinja2 templates
+templates = Jinja2Templates(directory="ui")
+
 # Serve index.html as the default page
-@app.get("/")
-async def read_index():
-    return FileResponse(os.path.join("ui", "index.html"))
+@app.get("/", response_class=FileResponse)
+async def read_index(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
