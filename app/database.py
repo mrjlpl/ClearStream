ff7cc654305407283607626cae8c0bbf448811a5
mrjl <mrjl@softleg.pl>
2025-01-05 18:50:46 +0100

    First working version

--- app/database.py
+++ app/database.py
@@ -3,6 +3,9 @@ from sqlmodel import SQLModel, create_engine, Session
 DATABASE_URL = "sqlite:///./test.db"
 engine = create_engine(DATABASE_URL, echo=True)
 
+# Define the Base class
+Base = SQLModel
+
 def get_session():
     with Session(engine) as session:
         yield session
